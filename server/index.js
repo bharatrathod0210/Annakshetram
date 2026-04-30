3c1f4d7068ddf0e5ce6d138c69137f47349fb261
Bharat Rathod <bharat.rathod0210@gmail.com>
2026-04-30 18:15:32 +0530

    payment gateway integration

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  // Respond to preflight immediately
+
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
   }
